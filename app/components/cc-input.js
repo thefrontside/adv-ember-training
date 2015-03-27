d0165acb8c413266a7d268eedc3b5c74fdc41905
Brandon Hays <brandon.hays@gmail.com>
2015-03-27 15:39:50 -0500

    move parsing to helper addon

--- app/components/cc-input.js
+++ app/components/cc-input.js
@@ -1,29 +1,10 @@
 import Ember from 'ember';
-import Parser from '../utils/card-parser';
 
-export default Ember.TextField.extend({
-  classNameBindings: ["parser.isValidCard"],
+import { CardInputParser } from 'adv-training-helpers';
 
-  parser: Ember.computed(function() {
-    return Parser.create();
-  }),
-
-  parseInput: Ember.observer('value', function() {
-    this.set('parser.input', this.get('value'));
-  }),
-
-  formatOutput: Ember.observer('parser.formattedOutput', function() {
-    this.set('value', this.get('parser.formattedOutput'));
-  }).on("init"),
-
-  setCreditCardType: Ember.observer('parser.type', function() {
-    this.set('type', this.get('parser.type'));
-  }),
-
-  setNumber: Ember.observer("parser.validNumber", function() {
-    this.set("number", this.get("parser.validNumber"));
-  }).on("didInsertElement"),
-
+export default Ember.TextField.extend(CardInputParser, {
+  classNameBindings: ["isValid"],
+  isValid: Ember.computed.reads("parser.isValidCard"),
   eventManager: {
     keyPress: function(e) {
       if (!String.fromCharCode(e.charCode).match(/\d/)) {
